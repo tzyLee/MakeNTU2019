6191a1aa61bc88dde8b9ac3fb638764f251a2c41
tzyLee <b06901145@ntu.edu.tw>
2019-03-30 17:13:21 +0800

    Merge Main and Entry

--- src/index.js
+++ src/index.js
@@ -3,4 +3,23 @@ import React from 'react';
 import Entry from './components/Entry';
 import Main from './components/Main';
 
-ReactDom.render(<Main />, document.getElementById('root'));
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { stage: 0 };
+    this.nextStage = this.nextStage.bind(this);
+  }
+
+  nextStage() {
+    this.setState({ stage: this.state.stage + 1 });
+  }
+
+  render() {
+    return React.createElement(App.states[this.state.stage], {
+      nextStage: this.nextStage
+    });
+  }
+}
+
+App.states = [Entry, Main];
+ReactDom.render(<App />, document.getElementById('root'));
